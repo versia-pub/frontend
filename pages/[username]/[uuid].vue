86e254b7e79019bf9cbec6eb3d8b307c5d706da5
Jesse Wierzbinski <contact@cpluspatch.com>
2024-04-27 21:46:05 -1000

    fix: :bug: Put the Note view on the app layout

--- pages/[username]/[uuid].vue
+++ pages/[username]/[uuid].vue
@@ -1,13 +1,13 @@
 <template>
     <ClientOnly>
-        <div v-if="note" class="max-w-2xl mx-auto md:py-20 md:px-10">
-            <SocialElementsNotesNote :note="note" />
-        </div>
+        <SocialElementsNotesNote v-if="note" :note="note" />
     </ClientOnly>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from "vue-router";
+definePageMeta({
+    layout: "app",
+})
 
 const route = useRoute();
 const client = useMegalodon();
