17441bfd4716fd6c6d31d51e2d032dac49497175
Jesse Wierzbinski <contact@cpluspatch.com>
2024-12-09 10:19:35 +0100

    feat: :zap: Add HTML spinner while loading frontend

---
+++ pages/loader.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="p-6 flex items-center justify-center h-dvh w-dvw">
+        <div class="flex flex-col items-center justify-center gap-4">
+            <Loader class="animate-spin size-8" />
+            <!-- <p class="text-xl font-semibold tracking-tight">Versia Frontend</p> -->
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { Loader } from "lucide-vue-next";
+</script>
