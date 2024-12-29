e12120853a8218e52d5e1e63797fb7025e7d7ee5
Jesse Wierzbinski <contact@cpluspatch.com>
2024-12-29 15:22:44 +0100

    refactor: :fire: Remove old code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,20 +3,6 @@ import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 import animate from "tailwindcss-animate";
 
-const themeVariables = (color: string) => ({
-    50: `var(--theme-${color}-50)`,
-    100: `var(--theme-${color}-100)`,
-    200: `var(--theme-${color}-200)`,
-    300: `var(--theme-${color}-300)`,
-    400: `var(--theme-${color}-400)`,
-    500: `var(--theme-${color}-500)`,
-    600: `var(--theme-${color}-600)`,
-    700: `var(--theme-${color}-700)`,
-    800: `var(--theme-${color}-800)`,
-    900: `var(--theme-${color}-900)`,
-    950: `var(--theme-${color}-950)`,
-});
-
 export default (<Config>{
     darkMode: "class",
     safelist: ["dark"],
